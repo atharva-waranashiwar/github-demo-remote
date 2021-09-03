1b35453bd402a4b28bb0564574b4bf595cea24a6
atharva_waranashiwar <atharva_waranashiwar@persistent.com>
2021-09-03 12:16:04 +0530

    added function header

--- math.py
+++ math.py
@@ -1,18 +1,18 @@
-# Add implementation
+# Add implementation fn header
 def add(x,y):
 	return(x+y)
-# subtract implementation
+# subtract implementation fn header
 def subtract(x,y):
 	return(x-y)
-#multiply implementation
+#multiply implementation fn header
 def multiply(x,y):
 	return(x*y)
-#divide implementation
+#divide implementation fn header
 def divide(x,y):
 	if y==0:
 		return DIVIDE_BY_0_ERROR
 	else:
 		return(x/y)
-#square implemetation
+#square implemetation fn header
 def square(x):
 	return(x*x)
