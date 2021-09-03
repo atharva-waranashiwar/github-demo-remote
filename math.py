c4eb87bb2ff6aaec4c45fe07d9be2df4e6e6f0a2
atharva-waranashiwar <90025235+atharva-waranashiwar@users.noreply.github.com>
2021-09-03 12:11:52 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,18 @@
+# Add implementation
+def add(x,y):
+	return(x+y)
+# subtract implementation
+def subtract(x,y):
+	return(x-y)
+#multiply implementation
+def multiply(x,y):
+	return(x*y)
+#divide implementation
+def divide(x,y):
+	if y==0:
+		return DIVIDE_BY_0_ERROR
+	else:
+		return(x/y)
+#square implemetation
+def square(x):
+	return(x*x)
